57d230f3800fc3e2fa3e8eb35de9deed7d30e016
tako <shukatsu34400@gmail.com>
2020-01-19 01:52:14 +0900

    Change host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'dry-temple-51563.herokuapp.com'
+  host = 'floating-mountain-10800.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
