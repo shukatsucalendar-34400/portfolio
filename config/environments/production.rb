cbb64bde8262b05bbcbb04cdfc025ff7b193414a
tako <shukatsu34400@gmail.com>
2020-01-19 02:12:45 +0900

    Add force_ssl to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,17 +14,13 @@ Rails.application.configure do
 
   config.assets.compile = false
 
+  config.force_ssl = true
+
   config.log_level = :debug
 
   config.log_tags = [ :request_id ]
 
   config.action_mailer.perform_caching = false
-
-  config.i18n.fallbacks = true
-
-  config.active_support.deprecation = :notify
-
-  config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
@@ -41,12 +37,17 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
   
+  config.i18n.fallbacks = true
+
+  config.active_support.deprecation = :notify
+
+  config.log_formatter = ::Logger::Formatter.new
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
